2ec1eb0fdfd251937ea39285e28a092924e63117
tambarie <gt.gbaragbo@gmail.com>
2022-03-04 00:55:05 +0100

    Six method signatures added to platformService interface

--- internal/ports/platform-service.go
+++ internal/ports/platform-service.go
@@ -1,3 +1,12 @@
 package ports
 
-type PlatformService interface{}
+import domain "platform-service/internal/core/domain/gas-platform-service"
+
+type PlatformService interface {
+	CreatePlatform(platform *domain.PlatformService) (*domain.PlatformService, error)
+	UpdatePlatform(platformReference string, platform *domain.PlatformService) (*domain.PlatformService, error)
+	GetCategoryByReference(reference string) ([]*domain.PlatformService, error)
+	GetCategoryByName(reference string) ([]*domain.PlatformService, error)
+	GetPlatformPage(page int64) ([]*domain.PlatformService, error, int64)
+	DeleteCategoryByReference(reference string) (interface{}, error)
+}
