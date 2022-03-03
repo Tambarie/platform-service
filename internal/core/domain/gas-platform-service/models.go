8a17b628c4d777b86a2ec0a1f44b77b2461b8b11
tambarie <gt.gbaragbo@gmail.com>
2022-03-04 00:53:24 +0100

    platform service struct added

--- internal/core/domain/gas-platform-service/models.go
+++ internal/core/domain/gas-platform-service/models.go
@@ -1,1 +1,7 @@
-package gas_platform_service
+package domain
+
+type PlatformService struct {
+	Reference   string `json:"reference,omitempty" bson:"reference"`
+	Name        string `validate:"required" json:"name,omitempty" bson:"name"`
+	Description string `json:"description,omitempty" bson:"description"`
+}
