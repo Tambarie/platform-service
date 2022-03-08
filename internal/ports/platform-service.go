fe2bc8face9d6ae5e790593d5ad39e1cc4bc27b5
tambarie <gt.gbaragbo@gmail.com>
2022-03-08 16:14:12 +0100

    add subcategory signatures to the platform service interface

--- internal/ports/platform-service.go
+++ internal/ports/platform-service.go
@@ -3,10 +3,17 @@ package ports
 import domain "platform-service/internal/core/domain/gas-platform-service"
 
 type PlatformService interface {
-	CreatePlatform(platform *domain.PlatformService) (*domain.PlatformService, error)
-	UpdatePlatform(platformReference string, platform *domain.PlatformService) (*domain.PlatformService, error)
-	GetCategoryByReference(reference string) ([]*domain.PlatformService, error)
-	GetCategoryByName(reference string) ([]*domain.PlatformService, error)
-	GetPlatformPage(page int64) ([]*domain.PlatformService, error, int64)
+	CreateCategory(platform *domain.Category) (*domain.Category, error)
+	UpdateCategory(platformReference string, platform *domain.Category) (*domain.Category, error)
+	GetCategoryByReference(reference string) ([]*domain.Category, error)
+	GetCategoryByName(reference string) ([]*domain.Category, error)
+	GetCategoryList(page int64) ([]*domain.Category, error, int64)
 	DeleteCategoryByReference(reference string) (interface{}, error)
+
+	CreateSubCategory(platform *domain.SubCategory) (*domain.SubCategory, error)
+	UpdateSubCategory(platformReference string, platform *domain.SubCategory) (*domain.SubCategory, error)
+	GetSubCategoryByReference(reference string) ([]*domain.SubCategory, error)
+	GetSubCategoryByName(reference string) ([]*domain.SubCategory, error)
+	GetSubCategoryList(page int64) ([]*domain.SubCategory, error, int64)
+	DeleteSubCategoryByReference(reference string) (interface{}, error)
 }
