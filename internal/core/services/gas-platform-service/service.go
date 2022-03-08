1e93c5c44555b87b411c0d7f102f84e6197b5941
tambarie <gt.gbaragbo@gmail.com>
2022-03-08 16:16:03 +0100

    change category methods and add sub-category methods on the service.go

--- internal/core/services/gas-platform-service/service.go
+++ internal/core/services/gas-platform-service/service.go
@@ -10,32 +10,60 @@ type Service struct {
 	platformRepository ports.PlatformRepository
 }
 
-func (s *Service) GetPlatformPage(page int64) ([]*domain.PlatformService, error, int64) {
-	return s.platformRepository.GetPlatformPage(page)
-}
-
-func (s *Service) GetCategoryByName(name string) ([]*domain.PlatformService, error) {
-	return s.platformRepository.GetCategoryByName(name)
-}
-
-func (s *Service) CreatePlatform(platform *domain.PlatformService) (*domain.PlatformService, error) {
+func (s *Service) CreateCategory(platform *domain.Category) (*domain.Category, error) {
 	if err := helper.Validate(platform); err != nil {
 		return nil, err
 	}
-	return s.platformRepository.CreatePlatform(platform)
+	return s.platformRepository.CreateCategory(platform)
 }
 
-func (s *Service) UpdatePlatform(platformReference string, platform *domain.PlatformService) (*domain.PlatformService, error) {
-	return s.platformRepository.UpdatePlatform(platformReference, platform)
+func (s *Service) UpdateCategory(platformReference string, platform *domain.Category) (*domain.Category, error) {
+	return s.platformRepository.UpdateCategory(platformReference, platform)
 }
 
-func (s *Service) GetCategoryByReference(reference string) ([]*domain.PlatformService, error) {
+func (s *Service) GetCategoryByReference(reference string) ([]*domain.Category, error) {
 	return s.platformRepository.GetCategoryByReference(reference)
+}
+
+func (s *Service) GetCategoryByName(name string) ([]*domain.Category, error) {
+	return s.platformRepository.GetCategoryByName(name)
+}
+
+func (s *Service) GetCategoryList(page int64) ([]*domain.Category, error, int64) {
+	return s.platformRepository.GetCategoryList(page)
 }
 
 func (s *Service) DeleteCategoryByReference(reference string) (interface{}, error) {
 	return s.platformRepository.DeleteCategoryByReference(reference)
 }
+
+func (s *Service) CreateSubCategory(platform *domain.SubCategory) (*domain.SubCategory, error) {
+	if err := helper.Validate(platform); err != nil {
+		return nil, err
+	}
+	return s.platformRepository.CreateSubCategory(platform)
+}
+
+func (s *Service) UpdateSubCategory(platformReference string, platform *domain.SubCategory) (*domain.SubCategory, error) {
+	return s.platformRepository.UpdateSubCategory(platformReference, platform)
+}
+
+func (s *Service) GetSubCategoryByReference(reference string) ([]*domain.SubCategory, error) {
+	return s.platformRepository.GetSubCategoryByReference(reference)
+}
+
+func (s *Service) GetSubCategoryByName(name string) ([]*domain.SubCategory, error) {
+	return s.platformRepository.GetSubCategoryByName(name)
+}
+
+func (s *Service) GetSubCategoryList(page int64) ([]*domain.SubCategory, error, int64) {
+	return s.platformRepository.GetSubCategoryList(page)
+}
+
+func (s *Service) DeleteSubCategoryByReference(reference string) (interface{}, error) {
+	return s.platformRepository.DeleteSubCategoryByReference(reference)
+}
+
 func New(platformRepository ports.PlatformRepository) *Service {
 	return &Service{platformRepository: platformRepository}
 }
